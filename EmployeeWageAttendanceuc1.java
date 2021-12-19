2d398379bb66ba05fc0c9806b6b939ad7f809154
saurabhyeggewar1998 <yeggewar@gmail.com>
2021-12-19 10:38:21 +0530

    Added : Welcome massage

---
+++ EmployeeWageAttendanceuc1.java
@@ -0,0 +1,16 @@
+
+class EmployeeWageAttendanceuc1 
+{
+	public static void main(String[] args) 
+	{
+	  int EMP = 1;
+	  double empcheck = Math.floor(Math.random() * 10) % 2;
+	  if (empcheck == EMP)
+	  {
+		  System.out.println("employee is present");
+	  }
+	  else {
+		  System.out.println("employee is absent");
+	  }
+	}
+}
